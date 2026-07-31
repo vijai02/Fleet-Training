d397bc872c820afb2f42ee81b1971f8e06167af2
Vijay Raj <154803153+vijai02@users.noreply.github.com>
2026-07-31 16:57:05 +0530

    Delete set-wallpaper.ps1

--- platforms/windows/configuration-profiles/set-wallpaper.ps1
+++
@@ -1,5 +0,0 @@
-$WallpaperPath = "C:\Path\To\Your\Image.jpg"
-$RegistryPath = "HKCU:\Control Panel\Desktop"
-
-Set-ItemProperty -Path $RegistryPath -Name Wallpaper -Value $WallpaperPath
-RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
